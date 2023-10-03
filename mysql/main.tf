720616d5df31e6784b48f606331fee654a017acf
Brett Swift <brettswift@gmail.com>
2023-10-03 08:39:09 -0600

    format

--- mysql/main.tf
+++ mysql/main.tf
@@ -27,7 +27,7 @@ terraform {
 resource "aws_db_instance" "mysql" {
   engine         = "mysql"
   engine_version = "5.6.41"
-  db_name = var.name 
+  db_name        = var.name
 
   username = var.master_username
   password = var.master_password
